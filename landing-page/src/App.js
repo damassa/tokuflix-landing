ae9cda9fefabeffc9926fdbbf815e4e426b0f54a
Felipe Leal Damasceno <felipelealdamasceno@gmail.com>
2021-02-15 21:29:12 -0300

    Styling landing page

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
+// import './global.css';
 
 function App() {
-  return <div className="App">This is a landing page</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ landing-page/src/components/Header/styles.js
@@ -0,0 +1,57 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  Header: {
+    backgroundImage:
+      "linear-gradient(to right bottom, #0082c1, #2b7cc7, #4b74ca, #686ac9, #845ec2)",
+  },
+  HeaderNav: {
+    padding: "20px 0",
+    "& button": {
+      padding: "5px 20px",
+      borderRadius: 100,
+      border: "none",
+      cursor: "pointer",
+      transition: ".2s",
+    },
+    "& button:hover": {
+      backgroundColor: "#CCCCCC",
+      transition: ".2s",
+    },
+  },
+  MenuItem: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  HeaderContent: {
+    padding: "20px 0 30px 0",
+    "& h3": {
+      color: "#FEFEFE",
+      fontSize: 28,
+    },
+    "& p": {
+      color: "#C4C4C4",
+    },
+    "& button": {
+      padding: "10px 30px",
+      borderRadius: 100,
+      border: "none",
+      cursor: "pointer",
+      transition: ".2s",
+    },
+    "& button:hover": {
+      backgroundColor: "#CCCCCC",
+      transition: ".2s",
+    },
+  },
+  HeaderImage: {
+    display: "flex",
+    justifyContent: "flex-end",
+    "& img": {
+      width: "70%",
+    },
+  },
+}));
+
+export default useStyles;
